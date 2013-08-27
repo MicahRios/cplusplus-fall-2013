aafe84b0e00c8463413a71c2ed014a348fb4e145
MicahRios <MrMicah317@juno.com>
2013-08-27 09:53:06 -0500

    Initial Coding for HelloWorld

---
+++ HelloWorld/HelloWorld/Main.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World";
+	cout << "\n\tC++ is the best";
+		// std::cout <<    IF "using namespace std;" is not used
+	_getch();
+
+
+	return 0;
+}
